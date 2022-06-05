3bc7a61b6d6821d4b84d97f3bb9902e3fdde49cf
NicoArdans <captainnico@gmail.com>
2022-06-05 10:16:43 -0700

    added csv generation
    csv file containing a table of times and units is now generated with a button press

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import PySimpleGUI as sg
 import DriverMeasure
 import math
+from pathlib import Path
+import csv
 
 sg.theme('GrayGrayGray')  # please make your windows colorful
 
@@ -80,6 +82,7 @@ layout = [[
 dm = DriverMeasure.DriverMeasure()
 
 textClick = False
+dataIsGenerated = False
 
 window = sg.Window('Picking Driver Measure Calculator', layout, size=(550, 300))
 while True:  # Event Loop=
@@ -96,21 +99,43 @@ while True:  # Event Loop=
         window["-shortCrew-"].update(dm.VALUESTREAMS["Short"]["crewSize"])
         window["-mediumCrew-"].update(dm.VALUESTREAMS["Medium"]["crewSize"])
         window["-longCrew-"].update(dm.VALUESTREAMS["Long"]["crewSize"])
+        dataIsGenerated = True
 
     if event == '-hourlyRate-':
-        if textClick==False:
+        if not textClick:
             textClick = True
             window['-hourlyRate-'].update("Daily Rate:")
             window['-shortRate-'].update(dm.VALUESTREAMS["Short"]["dailyRate"])
             window['-mediumRate-'].update(dm.VALUESTREAMS["Medium"]["dailyRate"])
             window['-longRate-'].update(dm.VALUESTREAMS["Long"]["dailyRate"])
-        elif textClick==True:
+        elif textClick:
             textClick = False
             window['-hourlyRate-'].update("Hourly Rate:")
             window['-shortRate-'].update(dm.VALUESTREAMS["Short"]["hourlyRate"])
             window['-mediumRate-'].update(dm.VALUESTREAMS["Medium"]["hourlyRate"])
             window['-longRate-'].update(dm.VALUESTREAMS["Long"]["hourlyRate"])
 
+    if event == '-generateExcel-':
+        if not dataIsGenerated:
+            sg.popup_ok("Please Calculate Crew Size First!")
+        elif dataIsGenerated:
+            file_path = sg.popup_get_file('Save as', no_window=True, save_as=True, file_types=(('Comma Separated Values', '*.csv'),))
+            if file_path:
+                dm.generate_driver_measure()
+                file = Path(file_path)
+                rows = []
+                for VS in dm.VALUESTREAMS:
+                    rows.append([str(VS)])
+                    rows.append(['Time', 'Target', 'Target Accumulative'])
+                    for key in range(1, 11):
+                        if len(dm.VALUESTREAMS[VS]["predictions"][key]) == 3:
+                            time_interval = str(dm.VALUESTREAMS[VS]["predictions"][key][0])
+                            target = str(dm.VALUESTREAMS[VS]["predictions"][key][1])
+                            target_accumulative = str(dm.VALUESTREAMS[VS]["predictions"][key][2])
+                            rows.append([time_interval, target, target_accumulative])
+                with open(file, 'w') as f:
+                    write = csv.writer(f)
+                    write.writerows(rows)
         # change the "output" element to be the value of "input" element
         # window['-OUTPUT-'].update(values['-IN-'])
         pass
