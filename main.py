8c035a663f312be2bdd5ec52df374f49b84c4b3f
NicoArdans <captainnico@gmail.com>
2022-06-05 20:09:14 -0700

    added default values for rates

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ layout = [[
                 SG.Text('Hourly Rate:', enable_events=True, key='-hourlyRate-')
             ],
             [
-                SG.Input(key='-shortRate-', size=5)
+                SG.Input(default_text="29", key='-shortRate-', size=5)
             ],
             [
-                SG.Input(key='-mediumRate-', size=5)
+                SG.Input(default_text="24", key='-mediumRate-', size=5)
             ],
             [
-                SG.Input(key='-longRate-', size=5),
+                SG.Input(default_text="15", key='-longRate-', size=5)
             ]
             ]),
         SG.Column([
